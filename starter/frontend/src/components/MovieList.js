fbf7d7f66097e1133dc1c38a3535dde17dbfe273
Kowshik <saimohankowshiktalluri@gmail.com>
2026-08-24 14:43:46 +0530

    Fix lint formatting in MovieList component

--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -6,7 +6,9 @@ function MovieList({ onMovieClick }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    const apiUrl = process.env.REACT_APP_MOVIE_API_URL || "http://a4604fa2070444ace825a73b1a3368a2-2070895866.us-east-1.elb.amazonaws.com";
+    const apiUrl =
+      process.env.REACT_APP_MOVIE_API_URL ||
+      'http://a4604fa2070444ace825a73b1a3368a2-2070895866.us-east-1.elb.amazonaws.com';
     axios.get(`${apiUrl}/movies`).then((response) => {
       setMovies(response.data.movies);
     });
@@ -15,7 +17,11 @@ function MovieList({ onMovieClick }) {
   return (
     <ul>
       {movies.map((movie) => (
-        <li className="movieItem" key={movie.id} onClick={() => onMovieClick(movie)}>
+        <li
+          className="movieItem"
+          key={movie.id}
+          onClick={() => onMovieClick(movie)}
+        >
           {movie.title}
         </li>
       ))}
