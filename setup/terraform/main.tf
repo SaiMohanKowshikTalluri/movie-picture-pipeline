f2f8e1514deb707e2119f680f7fbf1a38f102235
Kowshik <saimohankowshiktalluri@gmail.com>
2026-08-24 12:52:06 +0530

    Update frontend CI and CD workflow files

--- setup/terraform/main.tf
+++ setup/terraform/main.tf
@@ -142,7 +142,7 @@ resource "aws_ecr_repository" "backend" {
 # Create an EKS cluster
 resource "aws_eks_cluster" "main" {
   name     = "cluster"
-  version  = var.k8s_version
+  version  = "1.31"
   role_arn = aws_iam_role.eks_cluster.arn
   vpc_config {
     subnet_ids              = [aws_subnet.private_subnet.id, aws_subnet.public_subnet.id]
@@ -266,11 +266,11 @@ resource "aws_codebuild_project" "codebuild" {
   }
 
   environment {
-    compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:5.0"
-    type                        = "LINUX_CONTAINER"
+    compute_type                  = "BUILD_GENERAL1_SMALL"
+    image                         = "aws/codebuild/standard:5.0"
+    type                          = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
-    privileged_mode             = true
+    privileged_mode               = true
   }
 
   source {
@@ -310,21 +310,21 @@ resource "aws_iam_role_policy_attachment" "codebuild" {
 }
 
 ####################
-# Github Action role
+# Github Action role (Commented out to bypass AWS Academy IAM restrictions)
 ####################
-resource "aws_iam_user" "github_action_user" {
-  name = "github-action-user"
-}
+# resource "aws_iam_user" "github_action_user" {
+#   name = "github-action-user"
+# }
 
-resource "aws_iam_user_policy" "github_action_user_permission" {
-  user   = aws_iam_user.github_action_user.name
-  policy = data.aws_iam_policy_document.github_policy.json
-}
+# resource "aws_iam_user_policy" "github_action_user_permission" {
+#   user   = aws_iam_user.github_action_user.name
+#   policy = data.aws_iam_policy_document.github_policy.json
+# }
 
-data "aws_iam_policy_document" "github_policy" {
-  statement {
-    effect    = "Allow"
-    actions   = ["ecr:*", "eks:*", "ec2:*", "iam:GetUser"]
-    resources = ["*"]
-  }
-}
+# data "aws_iam_policy_document" "github_policy" {
+#   statement {
+#     effect    = "Allow"
+#     actions   = ["ecr:*", "eks:*", "ec2:*", "iam:GetUser"]
+#     resources = ["*"]
+#   }
+# }
