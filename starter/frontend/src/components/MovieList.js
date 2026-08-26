69d5fda45e641072adaf72f52c09ecb304ff15bb
Kowshik <saimohankowshiktalluri@gmail.com>
2026-08-26 12:13:57 +0530

    Update MovieList to handle direct array response from backend

--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -6,21 +6,29 @@ import axios from 'axios';
 function MovieList({ onMovieClick }) {
   const [movies, setMovies] = useState([]);
 
- useEffect(() => {
+  useEffect(() => {
     const apiUrl = process.env.REACT_APP_MOVIE_API_URL || 'http://afa9734f344c94017a3f906b6bd9d756-737657875.us-east-1.elb.amazonaws.com';
-    axios.get(`${apiUrl}/movies`).then((response) => {
-      setMovies(response.data.movies);
-    });
+    axios.get(`${apiUrl}/movies`)
+      .then((response) => {
+        // Handles both direct arrays and object wrappers just in case
+        const movieData = Array.isArray(response.data) ? response.data : response.data.movies;
+        setMovies(movieData || []);
+      })
+      .catch((error) => {
+        console.error("Error fetching movies:", error);
+      });
   }, []);
 
   return (
-    <ul>
-      {movies.map((movie) => (
-        <li className="movieItem" key={movie.id} onClick={() => onMovieClick(movie)}>
-          {movie.title}
-        </li>
-      ))}
-    </ul>
+    <div>
+      <ul>
+        {movies && movies.map((movie) => (
+          <li className="movieItem" key={movie.id} onClick={() => onMovieClick(movie)}>
+            {movie.title}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
